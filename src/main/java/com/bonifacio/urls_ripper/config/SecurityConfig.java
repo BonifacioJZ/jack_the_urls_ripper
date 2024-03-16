d1430185f48ca2330493e026753fbca8ee7190a8
Bonifacio Juarez Ceja <bonijuarezceja96@outlook.com>
2024-03-15 22:03:26 -0600

    swagger documentation and test urls

--- src/main/java/com/bonifacio/urls_ripper/config/SecurityConfig.java
+++ src/main/java/com/bonifacio/urls_ripper/config/SecurityConfig.java
@@ -1,6 +1,13 @@
 package com.bonifacio.urls_ripper.config;
 
 import com.bonifacio.urls_ripper.jwt.JwtAuthenticationFilter;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -43,6 +50,26 @@ public class SecurityConfig {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+
+    }
+    private SecurityScheme createAPIKeyScheme(){
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI().addSecurityItem(new SecurityRequirement()
+                .addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication",createAPIKeyScheme()))
+                .info(new Info().title("Jack The URLS Ripper")
+                        .description("App to create Short Links")
+                        .version("1.0").contact(new Contact().name("Bonifacio Juarez Ceja")
+                                .email("bonijuarezceja96@outlook.com"))
+                        .license(new License().name("License MIT")
+                                .url("https://github.com/BonifacioJZ/jack_the_urls_ripper/tree/main?tab=MIT-1-ov-file")));
+
 
     }
 }
